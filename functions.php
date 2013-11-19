50e626f17867c71e8fceed1f07c7ac09b11e5d90
Vitor George <vitor.george@gmail.com>
2013-11-19 19:31:29 -0200

    CSS file for this child theme

--- functions.php
+++ functions.php
@@ -12,8 +12,15 @@ function landquest_scripts() {
 	
 	// remove jeo site scripts
 	wp_deregister_script('jeo-site');
+	wp_deregister_style('jeo-main');
+	
+	// fonts
+	wp_register_style('font-ifdp', 'http://fonts.googleapis.com/css?family=IM+Fell+Double+Pica');
+	wp_register_style('font-galdeano', 'http://fonts.googleapis.com/css?family=Galdeano');
+	
+	// register child theme css
+	wp_enqueue_style('lanquest-main', get_stylesheet_directory_uri() . '/css/landquest-main.css', array('jeo-skeleton', 'jeo-lsf', 'font-opensans', 'font-ifdp', 'font-galdeano'), '0.0.1');
 	
 }
 add_action('wp_enqueue_scripts', 'landquest_scripts');
-
 ?>
