654e85bf1a1a004f11a585625902e5ba957621d4
Vitor George <vitor.george@gmail.com>
2013-11-25 12:36:41 -0200

    Split map data in eleven transients

--- inc/gdocs-to-map/gdocs-to-map.php
+++ inc/gdocs-to-map/gdocs-to-map.php
@@ -36,74 +36,73 @@ class LandQuest_GDocsToMap {
 		return array(
 			'flowers' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=0&output=csv',
 			'mow_irrigation' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=1&output=csv',
-			// 'mow_boreholes' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=2&output=csv',
-			// 'oxfam_sand_dams' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=3&output=csv',
-			// 'oxfam_boreholes' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=4&output=csv',
-			// 'oxfam_lakes' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=5&output=csv',
-			// 'oxfam_rivers' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=6&output=csv',
-			// 'oxfam_rock_catchments' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=7&output=csv',
-			// 'oxfam_springs' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=8&output=csv',
-			// 'oxfam_wells' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=9&output=csv',
-			// 'oxfam_earthpan' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=10&output=csv'
+			'mow_boreholes' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=2&output=csv',
+			'oxfam_sand_dams' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=3&output=csv',
+			'oxfam_boreholes' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=4&output=csv',
+			'oxfam_lakes' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=5&output=csv',
+			'oxfam_rivers' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=6&output=csv',
+			'oxfam_rock_catchments' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=7&output=csv',
+			'oxfam_springs' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=8&output=csv',
+			'oxfam_wells' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=9&output=csv',
+			'oxfam_earthpan' => 'https://docs.google.com/spreadsheet/pub?key=0AtAM3LpAmlqCdEtxRlYxalRiWlNpcTE4bFY3OU5Wb3c&single=true&gid=10&output=csv'
 		);
 	}
 
 	function get_data() {
+		
+		// read sources urls
+		$sources = $this->get_sources();
 
-		// delete_transient('landquest_map_data');
-		
-		$data = get_transient('landquest_map_data');
+		$data = array();
 
-		if(!$data) {
-			echo "Buscando...";
-			$sources = $this->get_sources();
-			
-			if(is_array($sources) && !empty($sources)) {
-		
-				$data = array();
-		
-				foreach($sources as $key => $csv) {
-					$csv = fopen($csv, 'r');
-					if(!$csv)
-						continue;
-						
-					$headers = $arr = $content = array();
+		foreach($sources as $key => $csv) {
+
+			// fetch transient data
+			$data[$key] = get_transient($key);
+
+			// if not available, retrieve it from google docs
+			if(!$data[$key]) {
+				
+				// initialize data array
+				$data[$key] = array();
+				
+				// open gdocs csv
+				$csv = fopen($csv, 'r');
+				if(!$csv)
+					continue;
 					
-					$i = 0;
-					while(($lineArray = fgetcsv($csv, 4000)) !== false) {
-						if(!is_array($data[$i]))
-							$arr[$i] = array();
-						for($j = 0; $j < count($lineArray); $j++) {
-							$arr[$i][$j] = $lineArray[$j];
-						}
-						$i++;
+				$headers = $arr = $content = array();
+				
+				$i = 0;
+				while(($lineArray = fgetcsv($csv, 4000)) !== false) {
+					if(!is_array($data[$i]))
+						$arr[$i] = array();
+					for($j = 0; $j < count($lineArray); $j++) {
+						$arr[$i][$j] = $lineArray[$j];
 					}
-					
-					$headers = $arr[0];
-					unset($arr[0]);
-					
-					$i = 0;
-					foreach($arr as $line) {
-						$j = 0;
-						foreach($line as $item) {
-							$content[$i][$headers[$j]] = $item;
-							$content[$i]['marker_class'] = $key;
-							$j++;
-						}
-						$i++;
-					}
-		
-					fclose($csv);
-					
-					
-					$data[$key] = $content;
-		
+					$i++;
 				}
-		
+				
+				$headers = $arr[0];
+				unset($arr[0]);
+				
+				$i = 0;
+				foreach($arr as $line) {
+					$j = 0;
+					foreach($line as $item) {
+						$content[$i][$headers[$j]] = $item;
+						$content[$i]['marker_class'] = $key;
+						$j++;
+					}
+					$i++;
+				}
+	
+				fclose($csv);
+				
+				$data[$key] = $content;	
+				
+				set_transient($key, $data[$key], 60*60*24);
 			}
-		
-			set_transient('landquest_map_data', $data, 60*60*24);
-		
 		}
 		
 		return $data;
